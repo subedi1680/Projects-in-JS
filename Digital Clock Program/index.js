32000b9b89f9dba07adb511710bd99211d53878b
subedi1680 <subedi1680@gmail.com>
2024-12-29 18:54:22 +0545

    Simple Digital Watch

---
+++ Digital Clock Program/index.js
@@ -0,0 +1,14 @@
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours.toString().padStart(2, '0')
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const time = `${hours}:${minutes}:${seconds} ${ampm}`;
+    document.getElementById('clock').innerText = time;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
